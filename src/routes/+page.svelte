7988b20c3d8a06dd3a32ffacfa0ddf0e162212b4
Kerry Wang <10476898+tecnd@users.noreply.github.com>
2024-06-16 02:21:48 -0500

    add image processing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,5 @@
   Visit <a href="https://kit.svelte.dev" class="text-blue-400">kit.svelte.dev</a
   > to read the documentation
 </p>
+
+<enhanced:img src="./dog.jpg" alt="This is a dog." class="w-96" />
